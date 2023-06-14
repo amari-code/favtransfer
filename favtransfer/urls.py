48b99bc7a7e636940280111ff233ce2a15a369c4
dlvga <antonio.delavega.mariano@gmail.com>
2023-06-14 14:57:02 +0200

    first release

--- favtransfer/urls.py
+++ favtransfer/urls.py
@@ -6,6 +6,6 @@ from .views import authorize_spotify, spotify_callback
 urlpatterns = [
     path('authorize-spotify/', authorize_spotify, name='authorize_spotify'),
     path('spotify-callback/', spotify_callback, name='spotify_callback'),
-    path('playlist', views.PlaylistFView.as_view(), name="playlist.new"),
+    path('playlist', views.PlaylistView.as_view(), name="playlist.new"),
     # path('playlist/view', views.PlaylistView.as_view(), name="playlist.view"),
 ]
